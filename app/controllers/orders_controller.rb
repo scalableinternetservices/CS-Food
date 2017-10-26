dd7d00ac3b1e6ed6c5530cec0498529608313c10
Zibin <zibin@ucsb.edu>
2017-10-26 14:32:26 -0700

    added editing feature for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,10 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def edit
+    @order = Order.find(params[:id])
+  end
+
   def create
     @order = Order.new(order_params)
      
@@ -22,7 +26,16 @@ class OrdersController < ApplicationController
 
   end
 
-  # snippet for brevity
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to @order
+    else
+      render 'edit'
+    end
+  end
+
   private
   def order_params
     params.require(:order).permit(:title, :text)
