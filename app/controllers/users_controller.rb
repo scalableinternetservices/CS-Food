70b45e2abd299ebd2ac45bf016f4435e479ed286
Zibin <zibin@ucsb.edu>
2017-11-07 10:40:27 -0800

    added profile bottom on home page after login, need some styling for the bottom position

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 
+  def index
+
+  end
+
   def new
-    
+
   end
 
   def show
-    @user = current_user
+    #@user = current_user
+    @user = User.find(params[:id])
   end
 
   def edit
