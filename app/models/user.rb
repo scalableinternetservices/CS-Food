ba9fa09ce8aa99569bda01f90f6b314b452815a7
Zibin <zibin@ucsb.edu>
2017-11-08 14:13:13 -0800

    added phone number format and birthday format

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :birthday, presence: true
-  validates :phone_number, presence: true
+  validates :birthday, presence: true, format: {with: /\d{4}\/\d{2}\/\d{2}/, message: "please use format YYYY/MM/DD"}
+  validates :phone_number, presence: true, format: { with: /\d{3}-\d{3}-\d{4}/, message: "please use format xxx-xxx-xxxx" }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
