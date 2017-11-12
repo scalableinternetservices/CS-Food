d99df8c33b343cad765418faf29c9d1579a5f804
John T. Alexander <jalexander.qed@gmail.com>
2017-11-11 17:38:47 -0800

    ja - create items and order_items association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+  has_many :order_items
+  has_many :items, through: :order_items
+  belongs_to :user
+  delegate :username, to: :user, prefix: true
+
   validates :title, presence: true,
             length: { minimum: 5, maximum: 30 }
   validates :points, presence: true,
@@ -9,7 +14,4 @@ class Order < ApplicationRecord
   def check_my_points_gte_order_points
     errors.add(:points, "ya goofed") if self.points > self.user.points
   end
-
-  belongs_to :user
-  delegate :username, to: :user, prefix: true
 end
