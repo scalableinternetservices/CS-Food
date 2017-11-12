7a40bb3e052f70d286617d921022fb6913a9c6cc
Zibin <zibin@ucsb.edu>
2017-11-11 18:20:58 -0800

    fix testing to exclude edit delete

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -27,22 +27,5 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     get item_url(@item)
     assert_response :success
   end
-
-  test "should get edit" do
-    get edit_item_url(@item)
-    assert_response :success
-  end
-
-  test "should update item" do
-    patch item_url(@item), params: { item: { name: @item.name } }
-    assert_redirected_to item_url(@item)
-  end
-
-  test "should destroy item" do
-    assert_difference('Item.count', -1) do
-      delete item_url(@item)
-    end
-
-    assert_redirected_to items_url
-  end
+  
 end
