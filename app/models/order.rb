e7ad84e0faf0f9103b2e251dc63e59e1f1fef51d
John T. Alexander <jalexander.qed@gmail.com>
2017-11-11 17:38:47 -0800

    ja - create items and order_items association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  has_many :order_items
+  has_many :items, through: :order_items
+  belongs_to :user
+
   validates :title, presence: true,
             length: { minimum: 5, maximum: 30 }
   validates :points, presence: true,
@@ -9,6 +13,4 @@ class Order < ApplicationRecord
   def check_my_points_gte_order_points
     errors.add(:points, "ya goofed") if self.points > self.user.points
   end
-
-  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109095422) do
+ActiveRecord::Schema.define(version: 20171112003127) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_items_on_name", unique: true
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "order_id"
+    t.integer "item_id"
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "title"
