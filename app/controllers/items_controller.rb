e194a87073d6bdb03df0e6d8e5f782dbae79badd
Zibin <zibin@ucsb.edu>
2017-11-11 18:18:08 -0800

    removed edit and delete items, fix some views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,14 +17,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  # GET /items/1/edit
-  def edit
-  end
-
   # POST /items
   # POST /items.json
   def create
-    @item = Item.new(item_params)
+    @item = Item.find_or_create_by(item_params)
 
     respond_to do |format|
       if @item.save
@@ -34,30 +30,6 @@ class ItemsController < ApplicationController
         format.html { render :new }
         format.json { render json: @item.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
-  def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /items/1
-  # DELETE /items/1.json
-  def destroy
-    @item.destroy
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
