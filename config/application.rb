2806ac8b7968a535e599cf6d9a7bbfebd5028261
John T. Alexander <jalexander.qed@gmail.com>
2017-11-30 17:58:33 -0800

    ja - create user on startup

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module CSFood
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.after_initialize do
+      u = User.create(username: 'testuser', email: 'testuser@gmail.com', password: 'test1234', first_name: 'testuse', last_name: 'testuser', birthday: '1900/01/01', phone_number: '456-456-4567')
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
